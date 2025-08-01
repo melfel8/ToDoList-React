c063b30f74aff3e604a7375cbda9298368bd0b19
rahmathidayat1203 <rh63800@gmail.com>
2025-08-01 07:55:28 +0700

    update

--- src/component/ToDoForm.js
+++ src/component/ToDoForm.js
@@ -1,32 +1,30 @@
 import React, { useState } from 'react';
 
-function ToDoForm({addTodo}) {
-
-    const [task, setTask] = useState("");
+function ToDoForm({ addTodo }) {
+  const [task, setTask] = useState("");
     
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!task) return;
-        addTodo(task);
-        setTask("");
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!task) return;
+    addTodo(task);
+    setTask("");
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input 
-            type='text'
-            className='todo-input'
-            value={task}
-            onChange={(e) => setTask(e.target.task)}
-            placeholder='Tambahkan tugas baru...'
-            />
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type='text'
+        className='todo-input'
+        value={task}
+        onChange={(e) => setTask(e.target.value)} // Perubahan di sini!
+        placeholder='Tambahkan tugas baru...'
+      />
 
-            <button type='submit' className='todo-button'>
-                Tambah
-            </button>
-        </form>
-    );
-
+      <button type='submit' className='todo-button'>
+        Tambah
+      </button>
+    </form>
+  );
 }
 
 export default ToDoForm;
